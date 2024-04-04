0f1ee4f986d6a305042aa5403b3bce7f5b734268
JamesKytosaho <james.erik.demir@gmail.com>
2024-04-04 21:13:09 +0200

    Store response

--- openai-test.py
+++ openai-test.py
@@ -1,13 +1,40 @@
+import json
 from openai import OpenAI
 client = OpenAI()
+
+prompt = "Give one activity suggestion for a day in Tokyo."
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    #{"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    #{"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-    {"role": "user", "content": "Hello"},
+    {"role": "user", "content":  prompt},
   ]
 )
 
-print(completion.choices[0].message)
+response_message = completion.choices[0].message
+model_used = completion.model
+
+print(response_message)
+
+json_file_path = "historical_responses.json"
+
+try:
+  with open(json_file_path, "r") as file: 
+    historical_responses = json.load(file)
+except FileNotFoundError:
+  print("file not found!")
+  historical_responses = []
+
+historical_responses["responses"].append({
+  "prompt": prompt,
+  "response_message": {
+    "content": response_message.content,
+    "role": response_message.role,
+    },
+  "model_used": model_used,
+  })
+
+with open(json_file_path, "w") as file:
+  json.dump(historical_responses, file, indent=4)
+  
+print("Response stored in historical_responses.json")
