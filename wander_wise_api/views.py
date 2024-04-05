75e6f4e0d6720e33e19102554acf490be2a37688
JamesKytosaho <james.erik.demir@gmail.com>
2024-04-05 21:58:56 +0200

    Rename openai file

--- wander_wise_api/views.py
+++ wander_wise_api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .actions import openai
+from .commands import suggestion
 
 # Create your views here.
 @api_view(['GET'])
 def suggestion(request): 
-    data = openai.generate_trip_suggestion()
+    data = suggestion.generate_trip_suggestion()
     return Response(data)
