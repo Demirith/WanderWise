87c6d3912ad093b90b53d5f16bb9553639e8fff0
JamesKytosaho <james.erik.demir@gmail.com>
2024-05-02 11:49:59 +0200

    Add enviroment variables in FE

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
 export const SCREEN_NAV_BAR_WIDTH_LIMIT = 768;
 
-export const WANDER_WISE_API = "http://127.0.0.1:8000/api/";
+export const WANDER_WISE_API = import.meta.env.VITE_WANDER_WISE_API;
 export const ENDPOINT_TRIPS_SUGGESTION = "trips/suggestion";
